3e5ac0d131bc4d1d4dccb07d39b568d31f04e81d
Eva <evams009@gmail.com>
2021-12-04 00:18:45 +0100

    Lab update

--- app.js
+++ app.js
@@ -12,17 +12,33 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Register the location for handlebars partials here:
-
-punkAPI
-  .getBeers()
-  .then(beersFromApi => console.log('Beers from the database: ', beersFromApi))
-  .catch(error => console.log(error));
-
-// Add the route handlers here:
-
 app.get('/', (req, res) => {
   res.render('index');
 });
+
+// Register the location for handlebars partials here:
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    console.log(beers[0])
+    res.render('beers', {
+      b: beers
+    })
+    .catch(error => console.log(error));
+  })
+});
+
+app.get('/random', (req,res) => {
+  punkAPI.getRandom()
+  .then(beers => {
+    res.render('random', {
+      beer: beers[0]
+    })
+    .catch(error => console.log(error));
+  })
+})
+
+// Add the route handlers here:
+
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
